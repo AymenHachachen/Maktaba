069b3b4e2aebdce4c70b31d069c37713ff625dba
Hich EM <hachachen2003@gmail.com>
2026-05-04 13:05:08 -0700

    TP5

--- TPs/app/src/main/java/com/ElOuedUniv/maktaba/MaktabaApp.kt
+++
@@ -1,7 +0,0 @@
-package com.ElOuedUniv.maktaba
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class MaktabaApp : Application()
--- TPs/app/src/main/java/com/ElOuedUniv/maktaba/presentation/book/detail/BookDetailUiAction.kt
+++
@@ -1,6 +0,0 @@
-package com.ElOuedUniv.maktaba.presentation.book.detail
-
-sealed class BookDetailUiAction {
-    object OnBackClick : BookDetailUiAction()
-    object OnDeleteClick : BookDetailUiAction()
-}
